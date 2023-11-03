376e8472c7e226653486e5c47631c3fbec346366
LukasWedberg <lew7761@nyu.edu>
2023-11-03 16:19:50 -0400

    RoadManager: I added a few new variables, and a new class for road segments

--- Assets/scripts/RoadManager.cs
+++ Assets/scripts/RoadManager.cs
@@ -4,14 +4,38 @@ using UnityEngine;
 
 public class RoadManager : MonoBehaviour
 {
+    public Camera cam;
 
+
+    public class Segment
+    {
+        float p1;
+        float p2;
+
+        Color segmentColor;
+
+
+        public Segment(int point1, int point2, Color thisColor) {
+            p1 = point1;
+            p2 = point2;
+            segmentColor = thisColor;
+        
+        }
+    }
+
+    float roadWidth = 10;
+
+
+
+
+    public Segment[] segments;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -19,4 +43,20 @@ public class RoadManager : MonoBehaviour
     {
         
     }
+
+
+    void ResetRoad() 
+    {
+        //for (int i = 0; i < 500; i++)
+        //{
+            //segments.Add(new Segment());
+        //}
+        
+    }
+
+    void RenderRoad() { 
+        
+
+    }
+
 }
