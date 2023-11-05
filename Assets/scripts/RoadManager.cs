0cc05361ee571a8fd695080b4e21c42fd7f0a72d
LukasWedberg <lew7761@nyu.edu>
2023-11-05 01:38:27 -0400

    RoadManager: I just have to figure out how to stretch our road texture, and we'll have a good-looking road that can be decorated! In the meantime, I made it so that the Psuedo-3D camera moves along the road at a set speed. I also optimized the code so that we can add stuff like decorations and different road sizes/textures/colors in later.

--- Assets/scripts/RoadManager.cs
+++ Assets/scripts/RoadManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class RoadManager : MonoBehaviour
 {
+    public Sprite defaultSegmentSprite;
+
     public Camera cam;
 
     float xAspect = 16;
@@ -13,24 +15,37 @@ public class RoadManager : MonoBehaviour
 
     float camWidth;
 
-    public float segmentLength;
+    public float ZPos = 0;
+
+    public float speed = .5f;
+
+    public float drawDistance = 12;
+
+    public float segmentLength = 2;
+
+    [SerializeField]
+    float trackLength;
+
+
 
 
     public class Segment
     {
-        Vector3 p1;
-        Vector3 p2;
+        public Vector3 p1;
+        public Vector3 p2;
 
-        Color segmentColor;
+        public Color segmentColor;
 
-        Sprite segmentSprite;
+        public Sprite segmentSprite;
+
+        public int index;
 
 
-        public Segment(Vector3 point1, Vector3 point2, Color thisColor) {
+        public Segment(Vector3 point1, Vector3 point2, Color thisColor, Sprite thisSegmentSprite) {
             p1 = point1;
             p2 = point2;
             segmentColor = thisColor;
-            
+            segmentSprite = thisSegmentSprite;
 
         }
     }
@@ -51,7 +66,7 @@ public class RoadManager : MonoBehaviour
 
 
 
-    public Segment[] segments;
+    public Segment[] segments = new Segment[500];
 
 
 
@@ -59,11 +74,16 @@ public class RoadManager : MonoBehaviour
     void Start()
     {
         camWidth = xAspect / yAspect * camHeight;
+
+        ResetRoad();
     }
 
     // Update is called once per frame
     void Update()
     {
+        ZPos = ZPos + speed * Time.deltaTime;
+
+        //Debug.Log(Mathf.Floor(ZPos / segmentLength) % segments.Length);
 
         RenderRoad();
     }
@@ -71,27 +91,60 @@ public class RoadManager : MonoBehaviour
 
     void ResetRoad() 
     {
-        //for (int i = 0; i < 500; i++)
-        //{
-            //segments.Add(new Segment());
-        //}
+        for (int i = 1; i < 501; i++)
+        {
+            //Debug.Log(new Segment(new Vector3(0, 0, i * segmentLength), new Vector3(0, 0, (i + 1) * segmentLength), Color.white, defaultSegmentSprite));
+            segments[i-1] = new Segment(new Vector3(0,0,i* segmentLength), new Vector3(0,0,(i+1)*segmentLength),Color.white,defaultSegmentSprite);
+            segments[i - 1].index = i - 1;
+        
+        }
+
+        trackLength = segments.Length * segmentLength;
+
+        Debug.Log(trackLength);
         
     }
 
     void RenderRoad() {
-        Debug.DrawLine( WorldToScreen(new Vector3(roadWidth/2,0,roadStart)), WorldToScreen(new Vector3(roadWidth/2, 0, roadEnd)));
+
+        Segment baseSegment = FindSegment(ZPos);
+        
+        //Debug.DrawLine( WorldToScreen(new Vector3(roadWidth/2,0,baseSegment.p1.z)), WorldToScreen(new Vector3(roadWidth/2, 0, roadEnd)));
         //Debug.Log(WorldToScreen(new Vector3(roadWidth/2, 0, roadStart)).x);
 
 
-        Debug.DrawLine(WorldToScreen(new Vector3(-roadWidth / 2, 0, roadStart)), WorldToScreen(new Vector3(-roadWidth / 2, 0, roadEnd)));
+        //Debug.DrawLine(WorldToScreen(new Vector3(-roadWidth / 2, 0, baseSegment.p1.z)), WorldToScreen(new Vector3(-roadWidth / 2, 0, roadEnd)));
         //Debug.Log(WorldToScreen(new Vector3(roadWidth / 2, 0, roadStart)).x);
 
+
+        for (int i = 0; i < drawDistance; i++)
+        {
+            Segment currentSegment = segments[(baseSegment.index + i) % segments.Length];
+
+            Debug.DrawLine(WorldToScreen(currentSegment.p1), WorldToScreen(currentSegment.p1 + new Vector3(0,0,.1f)));
+            Debug.DrawLine(WorldToScreen(currentSegment.p2), WorldToScreen(currentSegment.p2 + new Vector3(0,0,.1f)));
+
+            //Draw left and right segment bounds
+            Debug.DrawLine(WorldToScreen(currentSegment.p1 + new Vector3(roadWidth / 2, 0, 0)), WorldToScreen(currentSegment.p2 + new Vector3(roadWidth / 2, 0, 0)));
+
+            Debug.DrawLine(WorldToScreen(currentSegment.p1 + new Vector3(-roadWidth / 2, 0, 0)), WorldToScreen(currentSegment.p2 + new Vector3(-roadWidth / 2, 0, 0)));
+
+
+        }
+
+    }
+
+    Segment FindSegment(float currentZPosition) {
+
+        float segmentIndex = Mathf.Floor((Mathf.Floor(currentZPosition / segmentLength) % segments.Length) );
+
+        return segments[(int)segmentIndex];
     }
 
     Vector3 WorldToScreen(Vector3 worldPos) {
         float xCam = worldPos.x - cameraHorizontalOffset;
         float yCam = worldPos.y - cameraElevation;
-        float zCam = worldPos.z - 0;
+        float zCam = worldPos.z - ZPos;
 
         float xProj = xCam * (1/Mathf.Tan(FOV/2) ) / zCam;
         float yProj = yCam * (1/Mathf.Tan(FOV/2) ) / zCam;
